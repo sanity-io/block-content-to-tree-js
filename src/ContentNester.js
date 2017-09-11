697ffcad18ad575b3f748282f40e4897696756df
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-09-11 17:24:43 +0200

    Use a function instead of a class where applicable

--- src/ContentNester.js
+++ src/ContentNester.js
@@ -1,72 +1,58 @@
-class ContentNester {
-  constructor(spans) {
-    this.spans = spans
-  }
+module.exports = function nestContent(spans) {
+  const model = {content: []}
+  let nodeStack = [model]
 
-  run() {
-    const model = {content: []}
-    let nodeStack = [model]
+  spans.forEach(span => {
+    const dataAttributes = {...span}
+    delete dataAttributes.text
+    delete dataAttributes.marks
+    delete dataAttributes._type
 
-    this.spans.forEach(span => {
-      const dataAttributes = {...span}
-      delete dataAttributes.text
-      delete dataAttributes.marks
-      delete dataAttributes._type
+    const marksNeeded = span.marks.sort()
 
-      const marksNeeded = span.marks.sort()
+    let pos = 1
 
-      let pos = 1
-
-      // Start at position one. Root is always plain and should never be removed. (?)
-      if (nodeStack.length > 1) {
-        for (pos; pos < nodeStack.length; pos++) {
-          const mark = nodeStack[pos].mark
-          // eslint-disable-next-line max-depth
-          if (marksNeeded.includes(mark)) {
-            const index = marksNeeded.indexOf(mark)
-            marksNeeded.splice(index, 1)
-          } else {
-            break
-          }
+    // Start at position one. Root is always plain and should never be removed. (?)
+    if (nodeStack.length > 1) {
+      for (pos; pos < nodeStack.length; pos++) {
+        const mark = nodeStack[pos].mark
+        // eslint-disable-next-line max-depth
+        if (marksNeeded.includes(mark)) {
+          const index = marksNeeded.indexOf(mark)
+          marksNeeded.splice(index, 1)
+        } else {
+          break
         }
       }
+    }
 
-      // Keep from beginning to first miss
-      nodeStack = nodeStack.slice(0, pos)
+    // Keep from beginning to first miss
+    nodeStack = nodeStack.slice(0, pos)
 
-      // Add needed nodes
-      let currentNode = nodeStack.slice(-1)[0]
-      marksNeeded.forEach(mark => {
-        const node = {
-          content: [],
-          mark: mark,
-          type: 'span',
-          attributes: {}
-        }
-        currentNode.content.push(node)
-        nodeStack.push(node)
-        currentNode = node
-      })
-
-      if (Object.keys(dataAttributes).length) {
-        currentNode.content.push({
-          type: 'span',
-          attributes: dataAttributes,
-          content: [span.text]
-        })
-      } else {
-        currentNode.content.push(span.text)
+    // Add needed nodes
+    let currentNode = nodeStack.slice(-1)[0]
+    marksNeeded.forEach(mark => {
+      const node = {
+        content: [],
+        mark: mark,
+        type: 'span',
+        attributes: {}
       }
+      currentNode.content.push(node)
+      nodeStack.push(node)
+      currentNode = node
     })
 
-    return model.content
-  }
-}
+    if (Object.keys(dataAttributes).length) {
+      currentNode.content.push({
+        type: 'span',
+        attributes: dataAttributes,
+        content: [span.text]
+      })
+    } else {
+      currentNode.content.push(span.text)
+    }
+  })
 
-const getContent = spans => {
-  const cn = new ContentNester(spans)
-  const got = cn.run()
-  return got
+  return model.content
 }
-
-module.exports = getContent
