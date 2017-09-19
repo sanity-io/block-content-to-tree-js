c320f78381fb3731fc13d89939ab247fe0a34309
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-09-19 15:03:40 +0200

    Fix multi-mark handling

--- src/migrate.js
+++ src/migrate.js
@@ -1,6 +1,6 @@
 const objectAssign = require('object-assign')
 const generateKey = require('./generateKey')
-const knownSpanKeys = ['_type', 'text', 'marks']
+const knownSpanKeys = ['_type', '_key', 'text']
 
 function migrate(content, options) {
   if (content._type === 'block') {
@@ -21,25 +21,38 @@ function migrateBlock(content, options = {}) {
 
   const markDefs = []
   const children = content.spans.map(span => {
-    return Object.keys(span).reduce((child, key) => {
-      const knownKey = knownSpanKeys.includes(key)
-      if (knownKey) {
-        child[key] = span[key]
+    return Object.keys(span).reduce(
+      (child, key) => {
+        if (key === 'marks') {
+          return child
+        }
+
+        const knownKey = knownSpanKeys.includes(key)
+        if (knownKey) {
+          child[key] = span[key]
+          return child
+        }
+
+        // Only include "marks" that actually has content
+        const hasContent = Object.keys(span[key]).length > 0
+        if (!hasContent) {
+          return child
+        }
+
+        // Treat unknown keys as "custom" marks
+        const markKey = generateKey(span[key], options)
+        child.marks = [markKey].concat(span.marks)
+        markDefs.push(
+          objectAssign({}, span[key], {
+            _type: key,
+            _key: markKey
+          })
+        )
+
         return child
-      }
-
-      // Treat unknown keys as "custom" marks
-      const markKey = generateKey(span[key], options)
-      child.marks = [markKey].concat(span.marks)
-      markDefs.push(
-        objectAssign({}, span[key], {
-          _type: key,
-          _key: markKey
-        })
-      )
-
-      return child
-    }, {})
+      },
+      {marks: span.marks || []}
+    )
   })
 
   const migrated = objectAssign({}, content, {children, markDefs})
